0e2ed34026711d0e816cc70dde6cac090e72a52c
Joalbert González <joalbertgonzalez@gmail.com>
2017-11-16 21:24:05 -0500

    add to structure disabled, add event change and count

--- js/app.js
+++ js/app.js
@@ -4,23 +4,46 @@ function begin() {
   var tweetArea = document.getElementById('tweet-area');
   var tweetBtn = document.getElementById('tweet-btn');
   var messages = document.getElementById('messages');
+  var count = document.getElementById('count');
+  var MAXCHARACTERS = 140;
 
   tweetBtn.addEventListener('click', message);
+  tweetArea.addEventListener('keyup', changeText);
 
   function message(event) {
-    console.log(event.target.id);
     event.preventDefault();
 
-    var div = document.createElement('div');
-    var tweet = document.createElement('span');
+    if(tweetArea.value) {
+      var div = document.createElement('div');
+      var tweet = document.createElement('span');
 
-    tweet.textContent = tweetArea.value;
-    tweet.classList.add('tweet');
+      tweet.textContent = tweetArea.value;
+      tweet.classList.add('tweet');
 
-    div.classList.add('nuevo-mensaje');
-    tweetArea.value = '';
+      div.classList.add('nuevo-mensaje');
+      tweetArea.value = '';
+      tweetArea.focus();
 
-    div.appendChild(tweet);
-    messages.insertBefore(div, messages.firstElementChild);
+      div.appendChild(tweet);
+      messages.insertBefore(div, messages.firstElementChild);
+    } else {
+      event.target.disabled = true;
+    }
+  }
+
+  function changeText(event) {
+    console.log(event.target.value);
+
+    // si no existe, se asigna MAX
+    // si existe se habilita el boton y se resta el max con la longitud
+    if(event.target.value) {
+      tweetBtn.disabled = false;
+
+      if (event.target.value.length > 0)
+        count.textContent = MAXCHARACTERS - event.target.value.length;
+    } else {
+      tweetBtn.disabled = true;
+      count.textContent = MAXCHARACTERS;
+    }
   }
 }
